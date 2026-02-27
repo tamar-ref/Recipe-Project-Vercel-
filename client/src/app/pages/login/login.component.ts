41332d540c3c1bc2438712508fc0dbc3605fce8a
Tamar <tamar75262@gmail.com>
2026-02-27 10:36:37 +0200

    //

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,56 +1,56 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthService } from '../../shared/services/auth.service';
-import { UserService } from '../../shared/services/user.service';
+// import { Component } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { Router } from '@angular/router';
+// import { AuthService } from '../../shared/services/auth.service';
+// import { UserService } from '../../shared/services/user.service';
 
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatButtonModule } from '@angular/material/button';
 
-@Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [FormsModule, MatInputModule, MatButtonModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
-})
-export class LoginComponent {
-  password: string = '';
-  email: string = '';
-  message = '';
+// @Component({
+//   selector: 'app-login',
+//   standalone: true,
+//   imports: [FormsModule, MatInputModule, MatButtonModule],
+//   templateUrl: './login.component.html',
+//   styleUrl: './login.component.scss'
+// })
+// export class LoginComponent {
+//   password: string = '';
+//   email: string = '';
+//   message = '';
 
-  constructor(private router: Router, private authService: AuthService, private userService: UserService) { }
+//   constructor(private router: Router, private authService: AuthService, private userService: UserService) { }
 
-  login() {
-    this.userService.login({ email: this.email, password: this.password }).subscribe({
-      next: (res) => {
-        if (res.username) {
-          this.authService.setUser(res);
-        }
-        this.router.navigate(['/']);
-      },
-      error: (err) => {
-        alert(err.error.error)
-      }
-    });
-  }
+//   login() {
+//     this.userService.login({ email: this.email, password: this.password }).subscribe({
+//       next: (res) => {
+//         if (res.username) {
+//           this.authService.setUser(res);
+//         }
+//         this.router.navigate(['/']);
+//       },
+//       error: (err) => {
+//         alert(err.error.error)
+//       }
+//     });
+//   }
 
-  goToRegister() {
-    this.authService.email = this.email;
-    this.authService.password = this.password;
-    this.router.navigate(['/register']);
-  }
+//   goToRegister() {
+//     this.authService.email = this.email;
+//     this.authService.password = this.password;
+//     this.router.navigate(['/register']);
+//   }
 
-  forgotPassword() {
-    if (!this.email) {
-      alert('אנא הכנס/י את המייל שלך כדי לקבל קישור לאיפוס סיסמה');
-      return;
-    }
+//   forgotPassword() {
+//     if (!this.email) {
+//       alert('אנא הכנס/י את המייל שלך כדי לקבל קישור לאיפוס סיסמה');
+//       return;
+//     }
 
-    this.authService.forgotPassword(this.email).subscribe({
-      next: res => this.message = res.message,
-      error: err => this.message = err.error.message || 'שגיאה בשליחת הקישור'
-    });
-  }
+//     this.authService.forgotPassword(this.email).subscribe({
+//       next: res => this.message = res.message,
+//       error: err => this.message = err.error.message || 'שגיאה בשליחת הקישור'
+//     });
+//   }
 
-}
+// }

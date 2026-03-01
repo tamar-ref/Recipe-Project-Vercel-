1b52bb55ce16ef72cfc6c70c723d954643cbcb31
Tamar <tamar75262@gmail.com>
2026-03-01 18:54:47 +0200

    changes

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -3,6 +3,8 @@ import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../shared/services/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
@@ -19,7 +21,7 @@ export class NavbarComponent implements OnInit {
   currentUser: string = 'אורח';
   isAdmin: boolean = false;
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService, private dialog: MatDialog,) { }
 
   ngOnInit(): void {
     this.authService.currentUser$.subscribe(name => {
@@ -34,5 +36,18 @@ export class NavbarComponent implements OnInit {
     this.isAdmin = false;
     this.router.navigate(['/']);
   }
-  
+
+  confirmAction() {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: { message: 'האם להתנתק מהחשבון?' },
+      disableClose: false // מאפשר סגירה בלחיצה מחוץ לחלון
+    });
+
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result) {
+        this.logout();
+      }
+    });
+  }
+
 }
